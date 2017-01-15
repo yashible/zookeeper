944a5280215dcb427391fa0ed71d7975773f1e2c
Rui Fernando Hayashi <rfhayashi@gmail.com>
2017-01-15 13:28:11 -0200

    create zookeeper config file

--- test/integration/default/serverspec/zookeeper_spec.rb
+++ test/integration/default/serverspec/zookeeper_spec.rb
@@ -7,3 +7,8 @@ describe file('/usr/local/zookeeper/bin/zkServer.sh') do
   it { should exist }
   it { should be_file }
 end
+
+describe file('/usr/local/zookeeper/conf/zoo.cfg') do
+  it { should exist }
+  it { should be_file }
+end
